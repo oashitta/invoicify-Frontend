f186f9925d76a6ceb4b202857198779083292598
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2024-02-23 11:11:29 -0500

    added homepage component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import { useAuth0 } from '@auth0/auth0-react';
+import Homepage from './components/Homepage';
 
 function App() {
 
-  const { loginWithPopup, loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
-
   return (
-    <div style={{textAlign: "center"}}>
-    <h1>Billify Authentication</h1>
-    <ul>
-      <li style={{ textAlign: "center"}}>
-        <button onClick={loginWithPopup} style={{marginRight: '10px'}}>Login with Popup</button>
-        <button onClick={loginWithRedirect} style={{marginRight: '10px'}}>Login with Redirect</button>
-        <button onClick={logout}>Logout</button>
-        
-      </li>
-    </ul>
-
-    <h3> User is { isAuthenticated ? "Logged in" : " Not logged in"}</h3>
-    { isAuthenticated && (<pre style={{textAlign: 'start'}}> {JSON.stringify(user, null, 2)} </pre>)}
-
-    </div>
+   <div>
+    <Homepage/>
+   </div>
   );
 }
 
---
+++ src/components/Homepage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import '../css/Homepage.css'
+
+function Homepage() {
+
+  const { loginWithPopup, loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
+
+  return (
+    <div >
+    <h1>Billify Authentication</h1>
+    <ul>
+      <li >
+        <button onClick={loginWithPopup} >Login with Popup</button>
+        <button onClick={loginWithRedirect} >Login with Redirect</button>
+        <button onClick={logout}>Logout</button>
+        
+      </li>
+    </ul>
+
+    <h3> User is { isAuthenticated ? "Logged in" : " Not logged in"}</h3>
+    { isAuthenticated && (<pre style={{textAlign: 'start'}}> {JSON.stringify(user, null, 2)} </pre>)}
+
+    </div>
+    
+  );
+}
+
+export default Homepage;
