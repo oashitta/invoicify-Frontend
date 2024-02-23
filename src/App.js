30ceeb40d79ca2621565e99faa6f430837433f31
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2024-02-23 10:22:16 -0500

    implemented Auth0 service

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout';
-import Signin from './components/Signin';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
-  return (
-    <Routes>
-      <Route path='/' element= {<Layout />}>
-        <Route path='signin' element= {<Signin />}>
-        </Route>
-      </Route>
-    </Routes>
 
+  const { loginWithPopup, loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
+  return (
+    <div style={{textAlign: "center"}}>
+    <h1>Billify Authentication</h1>
+    <ul>
+      <li style={{ textAlign: "center"}}>
+        <button onClick={loginWithPopup} style={{marginRight: '10px'}}>Login with Popup</button>
+        <button onClick={loginWithRedirect} style={{marginRight: '10px'}}>Login with Redirect</button>
+        <button onClick={logout}>Logout</button>
+        
+      </li>
+    </ul>
+
+    <h3> User is { isAuthenticated ? "Logged in" : " Not logged in"}</h3>
+    { isAuthenticated && (<pre style={{textAlign: 'start'}}> {JSON.stringify(user, null, 2)} </pre>)}
+    
+    </div>
   );
 }
 
 export default App;
+// style={{display: "block", marginBottom: "10px" }}
--- src/components/Signin.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Signin = () => {
-  return (
-    <h1>Sign-in</h1>
-  )
-}
-
-export default Signin
