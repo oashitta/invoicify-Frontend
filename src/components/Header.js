0bc1803dc6de4257a5728b3538b60ca371d81504
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2024-03-14 10:35:26 -0400

    updated env example

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import axios from "axios";
 import '../css/Homepage.css'
 import LoginButton from './Login';
 import LogoutButton from './Logout';
@@ -10,12 +11,30 @@ function Header() {
 
   const { user, isAuthenticated } = useAuth0();
 
+  function callApi() {
+    axios.get("http://localhost:3500/").then(response => console.log(response.data)).catch(error => console.log(error.message))
+  }
+
+  function callProtectedApi() {
+    axios.get("http://localhost:3500/protected").then(response => console.log(response.data)).catch(error => console.log(error.message))
+  }
+
   return (
     <div >
      
       <h1>Billify Authentication</h1>
       <LoginButton />
       <LogoutButton />
+
+      <ul>
+        <li >
+          <button onClick={callApi}>Call API Route</button>
+        </li>
+        <li >
+          <button onClick={callProtectedApi}>Call Protected API Route</button>
+        </li>
+      </ul>
+
       <h3> User is { isAuthenticated ? "Logged in" : " Not logged in"}</h3>
       { isAuthenticated && (<pre style={{textAlign: 'start'}}> {JSON.stringify(user, null, 2)} </pre>)}
 
